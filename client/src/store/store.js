724b158c5ca55167cf4e37e51270036969724472
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-01 21:18:59 +0530

    created redux store

---
+++ client/src/store/store.js
@@ -0,0 +1,17 @@
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from "redux";
+import thunk from "redux-thunk";
+import authReducer from "./auth/auth.reducer";
+import infoReducer from "./info/info.reducer";
+
+const reducer = combineReducers({ auth: authReducer, info: infoReducer });
+const logger =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, compose(logger, applyMiddleware(thunk)));
+
+export default store;
