a7d72dd3c71a47a65bbaad01b61aa987820c6068
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-03 13:13:23 +0530

    added error boundary component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./Index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store/store";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const Main = function () {
-  return (
-    <Provider store={store}>
-      <Router>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </Router>
-    </Provider>
-  );
+	return (
+		<ErrorBoundary>
+			<Provider store={store}>
+				<Router>
+					<ChakraProvider>
+						<App />
+					</ChakraProvider>
+				</Router>
+			</Provider>
+		</ErrorBoundary>
+	);
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
