8a3007f61596708d397c882971ae67e9a7e62a9d
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-05 13:14:40 +0530

    created transactions model

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,22 +1,23 @@
 const { Schema, model } = require("mongoose");
+const Transactions = require("./transactions.model");
 
 const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    transactions: [{ type: Schema.Types.ObjectId, ref: Transactions }],
-  },
-  { versionKey: false, timestamps: { createdAt: true, updatedAt: true } }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		transactions: [{ type: Schema.Types.ObjectId, ref: Transactions }],
+	},
+	{ versionKey: false, timestamps: { createdAt: true, updatedAt: true } }
 );
 
 const User = model("user", userSchema);
