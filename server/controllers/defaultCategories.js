f3ae645ae4ac8c44e5d4521489aabf673b36140d
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-06 21:45:02 +0530

    created controller functions for user signup

---
+++ server/controllers/defaultCategories.js
@@ -0,0 +1,13 @@
+const Category = require("../model/category.model");
+
+async function getDefaultCategories() {
+	try {
+		let categories = await Category.find({ created_b: "admin" });
+		return categories;
+	} catch (e) {
+		console.log("ERROR IN FETCHING DEFAULT CATEGORIES", e.message);
+		return new Error(e.message);
+	}
+}
+
+module.exports = getDefaultCategories;
