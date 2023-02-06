9c525d2bce858117a245609f3fbd96165c34f587
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-06 23:42:49 +0530

    created login route for user

---
+++ server/routes/login.routes.js
@@ -0,0 +1,20 @@
+const generateToken = require("../controllers/generateToken");
+const loginUser = require("../controllers/loginUser");
+const loginValidator = require("../middlewares/login.middlewares");
+const login = require("express").Router();
+
+login.use(loginValidator);
+
+login.post("/", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		let id = await loginUser(email, password);
+		const token = generateToken(id);
+		return res.status(200).send({ ok: true, token });
+	} catch (e) {
+		console.log("ERROR IN LOGIN ROUTE", e.message);
+		res.status(400).send({ ok: false, message: e.message });
+	}
+});
+
+module.exports = login;
