9d0d772d1ef7b698548809aeca5acb996e1cbee2
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-07 20:51:09 +0530

    updated info reducer

--- client/src/store/info/info.reducer.js
+++ client/src/store/info/info.reducer.js
@@ -1,50 +1,52 @@
 import {
-  LOADING,
-  ERROR,
-  SUCCESS,
-  MESSAGE,
-  CLEAR_MESSAGE,
+	LOADING,
+	ERROR,
+	SUCCESS,
+	MESSAGE,
+	CLEAR_MESSAGE,
 } from "./info.actionTypes";
 
 const initState = {
-  error: false,
-  success: false,
-  loading: false,
-  message: "",
+	error: false,
+	success: false,
+	loading: false,
+	message: "",
 };
 
 const infoReducer = (state = initState, { type, payload }) => {
-  switch (type) {
-    case LOADING:
-      return {
-        ...state,
-        loading: payload,
-      };
-    case ERROR:
-      return {
-        ...state,
-        error: payload,
-      };
-    case SUCCESS:
-      return {
-        ...state,
-        success: payload,
-      };
+	switch (type) {
+		case LOADING:
+			return {
+				...state,
+				loading: payload,
+			};
+		case ERROR:
+			return {
+				...state,
+				error: payload,
+			};
+		case SUCCESS:
+			return {
+				...state,
+				success: payload,
+			};
 
-    case MESSAGE:
-      return {
-        ...state,
-        message: "",
-      };
+		case MESSAGE:
+			return {
+				...state,
+				message: payload,
+			};
 
-    case CLEAR_MESSAGE:
-      return {
-        ...state,
-        message: "",
-      };
+		case CLEAR_MESSAGE:
+			return {
+				...state,
+				message: "",
+				error: false,
+				success: false,
+			};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
 export default infoReducer;
