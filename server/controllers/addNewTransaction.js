b5a35252e407f7edea5dd6a1f2d5c3b5c88ff605
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-09 01:42:40 +0530

    removed log statement

--- server/controllers/addNewTransaction.js
+++ server/controllers/addNewTransaction.js
@@ -17,7 +17,6 @@ async function addNewTransaction({ type, category, amount, date }, userId) {
 		await User.findByIdAndUpdate(userId, {
 			$push: { transactions: newTransaction._id },
 		});
-		console.log(new Date(date).toString());
 	} catch (e) {
 		console.log("ERROR IN CREATING NEW TRANSACTION", e.message);
 		throw new Error(e.message);
