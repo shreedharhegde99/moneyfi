ad494777a1661fd9102bbe05699af1cf6bf74052
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-09 15:16:25 +0530

    modified transaction controller functions

--- server/controllers/removeTransaction.js
+++ server/controllers/removeTransaction.js
@@ -1,8 +1,10 @@
 const Transactions = require("../model/transactions.model");
+const User = require("../model/user.model");
 
-async function removeTransaction(id) {
+async function removeTransaction(id, userId) {
 	try {
 		await Transactions.findByIdAndRemove(id);
+		await User.findByIdAndUpdate(userId, { $pull: { transactions: id } });
 	} catch (e) {
 		console.log("ERROR IN REMOVING TRANSACTION", e.message);
 		throw new Error(e.message);
