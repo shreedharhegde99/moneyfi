53282e9e5d1ac75b57207870e27983f991d3c857
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-09 15:17:02 +0530

    created category route

--- server/routes/categories.routes.js
+++ server/routes/categories.routes.js
@@ -1,19 +1,44 @@
-const getUser = require("../controllers/getUserCategories");
+const addNewCategory = require("../controllers/addNewCategory");
+const getUserCategories = require("../controllers/getUserCategories");
+const removeCategory = require("../controllers/removeCategory");
 const validateUser = require("../controllers/validateUser");
 
-const dashboard = require("express").Router();
+const categoryRoute = require("express").Router();
 
-dashboard.use(validateUser);
+categoryRoute.use(validateUser);
 
-dashboard.get("/", async (req, res) => {
+categoryRoute.get("/", async (req, res) => {
 	try {
 		const { id } = req;
 
-		let user = await getUser(id);
+		let user = await getUserCategories(id);
 		return res.status(200).send({ ok: true, user });
 	} catch (e) {
-		console.log("ERROR IN FETCHING USER DETAILS", e.message);
+		console.log("ERROR IN FETCHING CATEGORY LIST", e.message);
 	}
 });
 
-module.exports = dashboard;
+categoryRoute.post("/", async (req, res) => {
+	try {
+		await addNewCategory(req.body, req.id);
+		res.status(201).send({ ok: true, message: "Category added successfully" });
+	} catch (e) {
+		console.log("ERROR IN CATEGORY POST METHOD", e.message);
+		return res.status(500).send({ ok: false, message: e.message });
+	}
+});
+
+categoryRoute.delete("/:categoryId", async (req, res) => {
+	try {
+		const { categoryId } = req.params;
+		await removeCategory(categoryId, req.id);
+		res
+			.status(200)
+			.send({ ok: true, message: "Category removed successfully" });
+	} catch (e) {
+		console.log("ERROR IN CATEGORY DELETE METHOD", e.message);
+		return res.status(500).send({ ok: false, message: e.message });
+	}
+});
+
+module.exports = categoryRoute;
