7b5b2836c1bcfc855a43b5d569afa83e7a48e6a1
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-14 23:35:04 +0530

    created user reducer and updated store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,8 +7,13 @@ import {
 import thunk from "redux-thunk";
 import authReducer from "./auth/auth.reducer";
 import infoReducer from "./info/info.reducer";
+import userReducer from "./user/user.reducer";
 
-const reducer = combineReducers({ auth: authReducer, info: infoReducer });
+const reducer = combineReducers({
+	auth: authReducer,
+	info: infoReducer,
+	user: userReducer,
+});
 const logger =
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
