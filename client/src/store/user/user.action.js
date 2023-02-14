ebb9ce7cf9109823eaf49f8bd6488d4bf7f688a9
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-14 23:34:39 +0530

    created transaction actions

--- client/src/store/user/user.action.js
+++ client/src/store/user/user.action.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 import baseURL from "../../network/network";
 import getToken from "../../utils/getToken";
+import {
+	setErrorStatus,
+	setLoadingStatus,
+	setMessage,
+} from "../info/info.action";
 import { CATEGORIES, TRANSACTIONS } from "./user.actionTypes";
 
 const token = getToken();
@@ -26,22 +31,27 @@ const getCategories = () => async (dispatch) => {
 			headers: {
 				Authorization: token,
 			},
-		});
+		}).then((res) => res.data);
+
 		dispatch(setCategories(categories));
+		dispatch(setUserDetails(name));
 	} catch (e) {
 		console.log("ERROR IN FETCHING USER CATEGORIES", e.message);
 		dispatch(setLoadingStatus(false));
-		dispatch(setErrorStatus(true));
-		dispatch(setMessage(e.response.data.message));
 	}
 };
 
 const addNewCategory = (payload) => async (dispatch) => {
+	dispatch(setLoadingStatus(true));
 	try {
 		let res = await instance({
 			method: "post",
 			url: "/categories",
-		});
+			data: payload,
+		}).then((res) => res.data);
+		dispatch(setLoadingStatus(false));
+		dispatch(setSuccessStatus(true));
+		dispatch(setMessage(res.message));
 	} catch (e) {
 		console.log("ERROR IN ADDING NEW CATEGORY", e.message);
 		dispatch(setLoadingStatus(false));
@@ -51,7 +61,9 @@ const addNewCategory = (payload) => async (dispatch) => {
 };
 
 const removeCategory = (id) => async (dispatch) => {
+	dispatch(setLoadingStatus(true));
 	try {
+		dispatch(setLoadingStatus(true));
 	} catch (e) {
 		console.log("ERROR IN DELETING THE CATEGORY", e.message);
 		dispatch(setLoadingStatus(false));
@@ -60,4 +72,83 @@ const removeCategory = (id) => async (dispatch) => {
 	}
 };
 
-export { getCategories, addNewCategory, removeCategory };
+const getTransactions = () => async (dispatch) => {
+	dispatch(setLoadingStatus(true));
+	try {
+		let res = await instance({
+			url: "/transactions",
+		}).then((res) => res.data);
+		dispatch(setLoadingStatus(false));
+		dispatch(setTransactions(res.data));
+	} catch (e) {
+		console.log("ERROR IN FETCHING", e.message);
+		dispatch(setLoadingStatus(false));
+		dispatch(setErrorStatus(true));
+		dispatch(setMessage(e.response.data.message));
+	}
+};
+
+const addNewTransaction = (payload) => async (dispatch) => {
+	dispatch(setLoadingStatus(true));
+	try {
+		let res = await instance({
+			method: "post",
+			url: "/transactions",
+			data: payload,
+		}).then((res) => res.data);
+
+		dispatch(setLoadingStatus(false));
+		dispatch(setMessage(res.message));
+	} catch (e) {
+		console.log("ERROR IN FETCHING", e.message);
+		dispatch(setLoadingStatus(false));
+		dispatch(setErrorStatus(true));
+		dispatch(setMessage(e.response.data.message));
+	}
+};
+
+const updateTransaction = (payload) => async (dispatch) => {
+	dispatch(setLoadingStatus(true));
+	try {
+		let res = await instance({
+			method: "patch",
+			url: "/transactions",
+			data: payload,
+		}).then((res) => res.data);
+
+		dispatch(setLoadingStatus(false));
+		dispatch(setMessage(res.message));
+	} catch (e) {
+		console.log("ERROR IN FETCHING", e.message);
+		dispatch(setLoadingStatus(false));
+		dispatch(setErrorStatus(true));
+		dispatch(setMessage(e.response.data.message));
+	}
+};
+const removeTransaction = (id) => async (dispatch) => {
+	dispatch(setLoadingStatus(true));
+	try {
+		let res = await instance({
+			method: "patch",
+			url: `/transactions/${id}`,
+		}).then((res) => res.data);
+
+		dispatch(setLoadingStatus(false));
+		dispatch(setMessage(res.message));
+	} catch (e) {
+		console.log("ERROR IN FETCHING", e.message);
+		dispatch(setLoadingStatus(false));
+		dispatch(setErrorStatus(true));
+		dispatch(setMessage(e.response.data.message));
+	}
+};
+
+export {
+	getCategories,
+	addNewCategory,
+	removeCategory,
+	getTransactions,
+	addNewTransaction,
+	updateTransaction,
+	removeTransaction,
+};
