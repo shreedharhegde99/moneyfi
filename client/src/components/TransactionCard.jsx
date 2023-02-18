295bdf6ec8b77c0ddeb6ed68dac2973023af416a
Shreedhar Hegde <snhkelemane@gmail.com>
2023-02-18 16:18:22 +0530

    modified style and added daily total display

--- client/src/components/TransactionCard.jsx
+++ client/src/components/TransactionCard.jsx
@@ -1,22 +1,39 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { Fragment } from "react";
 import { BiRupee } from "react-icons/bi";
-
+import getDailyTotal from "../utils/getDailyTotal";
 import getDate from "../utils/getDate";
 
 export default function TransactionCard({ data }) {
+	let total = getDailyTotal(data.transactions);
 	return (
 		<Fragment>
-			<Box py="2">
-				<Box>{getDate(data.date)}</Box>
+			<Box py="5">
+				<Flex justify="space-between" fontSize="md" fontWeight="bold" px="2">
+					<Box>{getDate(data.date)}</Box>
+					<Box color={total >= 0 ? "green.300" : "red.300"}>
+						{`${total >= 0 ? "+" : ""}${total}`}
+					</Box>
+				</Flex>
 				<Box>
 					{data.transactions.map((el) => (
-						<Flex justify="space-between" key={el._id}>
-							<Flex>
-								<Box>{el.type === "income" ? "+" : "-"}</Box>
+						<Flex
+							justify="space-between"
+							align="center"
+							key={el._id}
+							bg={el.type === "income" ? "green.100" : "red.100"}
+							p="2"
+							px="4"
+							my="2"
+							borderRadius="2xl"
+						>
+							<Flex gap="4">
+								<Box fontWeight="semibold">
+									{el.type === "income" ? "+" : "-"}
+								</Box>
 								<Box>{el.category}</Box>
 							</Flex>
-							<Flex>
+							<Flex align="baseline" gap="1">
 								<BiRupee />
 								{el.amount}
 							</Flex>
